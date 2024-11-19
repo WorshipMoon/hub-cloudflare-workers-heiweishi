de03c6eaac4e6a3b05c7601aaa8be2c101260ec4
Moon <moon@163.com>
2024-11-19 22:26:53 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,48 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-
+import { createClient } from '@supabase/supabase-js';
 export default {
+	// async fetch(request, env) {
+	// 	const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
+	// 	const { data, error } = await supabase.from('countries').select('*');
+	// 	if (error) throw error;
+	// 	return new Response(JSON.stringify(data), {
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+	// 		},
+	// 	});
+	// },
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		// console.log('request', request);
+		console.log('env', env);
+
+		// const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
+		// const { data, error } = await supabase.from('vpn_link').select('*');
+		// if (error) throw error;
+		// return new Response(JSON.stringify(data), {
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 	},
+		// });
+		let { pathname } = new URL(request.url);
+		switch (pathname) {
+			case '/hello':
+				return new Response('Hello World!');
+			case '/about':
+				return new Response('About Page');
+			case '/contact':
+				return new Response('Contact Page');
+			default:
+				// 默认路由
+				return new Response('Home', { status: 200 });
+
+				return new Response('Not Found', { status: 404 });
+		}
+
+		// return new Response('Hello World!');
+	},
+	async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
+		console.log('cron processed');
 	},
 } satisfies ExportedHandler<Env>;
